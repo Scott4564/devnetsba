773e3e758124f784f3355af1af259144f9554887
devnetsba <gordon.michael@spcollege.edu>
2023-11-28 17:39:47 -0500

    Mike Gordon

--- docker/app.sh
+++
@@ -1,4 +0,0 @@
-# Read input using read command 
-read -p "May I know your name please? " name 
-echo "Hello $name, let us be friends" 
-echo "" 
--- restapi/credentials.py
+++
@@ -1,2 +0,0 @@
-username = "nysig132"
-password = "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" 
